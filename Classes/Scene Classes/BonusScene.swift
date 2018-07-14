1edd28823a1e7afa7737618f8c8270af0bc6e231
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-07-14 16:40:36 -0700

    refactored addDrinkMatrix function

--- Classes/Scene Classes/BonusScene.swift
+++ Classes/Scene Classes/BonusScene.swift
@@ -77,7 +77,12 @@ class BonusScene: SKScene, SKPhysicsContactDelegate {
     func createPlayer() {
         player = Player(imageNamed: "flying1");
         player.initialize();
-        player.position = CGPoint(x: 0, y: 0);
+        
+        let x = CGFloat(-(self.frame.size.width/2) + self.frame.size.width/6);
+        let y = CGFloat(self.frame.size.height/2 - 3 * (self.frame.size.height/4));
+        
+//        player.position = CGPoint(x: -480, y: -230);
+        player.position = CGPoint(x: x, y: y);
         
         self.addChild(player);
         player.fly();
@@ -85,30 +90,32 @@ class BonusScene: SKScene, SKPhysicsContactDelegate {
     
     func addDrinkMatrix() {
         
-        var x = CGFloat(-480);
-        var y = CGFloat(180);
-        var count = 0;
+        var x = CGFloat(-(self.frame.size.width/2) + self.frame.size.width/6);
+        var y = CGFloat(self.frame.size.height/2 - self.frame.size.height/4);
         
-        // 3 rows
-        for _ in 1...3 {
-            // 5 columns
-            for _ in 1...5 {
+        // 3 rows and 5 columns of drinks
+        for i in 1...3 {
+            for j in 1...5 {
+                
+                if (i == 3 && j == 1) {
+                    // do nothing bc player will be in this area
+                } else {
         
-                drink = Drink(imageNamed: "drink");
+                    drink = Drink(imageNamed: "drink");
         
-                let referencePosition = CGPoint(x: x, y: y);
-                let offsetYValue = CGFloat(0);
+                    let referencePosition = CGPoint(x: x, y: y);
+                    let offsetYValue = CGFloat(0);
             
-                drink.initialize(referencePosition: referencePosition, offsetYValue: offsetYValue);
-            
-                self.addChild(drink);
-        
-                x += 2.5 * drink.size.width;
+                    drink.initialize(referencePosition: referencePosition, offsetYValue: offsetYValue);
+                    self.addChild(drink);
+                }
+                
+                x += self.frame.size.width/6;
             }
-        
-            y -= 2.5 * drink.size.height;
-            x = -480
-        }
+            
+            y -= (self.frame.size.height/2 - self.frame.size.height/4);
+            x = (-(self.frame.size.width/2) + self.frame.size.width/6);
+        };
     }
     
 }
