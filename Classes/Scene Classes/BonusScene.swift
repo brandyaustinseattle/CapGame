58cb034d12a937bc73769c121cba14f93926878f
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-07-14 17:16:14 -0700

    build out createClouds function so clouds are randomized

--- Classes/Scene Classes/BonusScene.swift
+++ Classes/Scene Classes/BonusScene.swift
@@ -47,17 +47,38 @@ class BonusScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createClouds() {
-        for i in 1...3 {
+        
+        let cloudSizeArray = [CGFloat(0.55), CGFloat(0.8), CGFloat(1.05)];
+        
+        var x = CGFloat(self.frame.size.width/2);
+        var y = CGFloat(self.frame.size.height/2 - (self.frame.size.height/4));
+        
+        for _ in 1...2 {
             let cloud = SKSpriteNode(imageNamed: "cloud");
             cloud.name = "cloud";
             cloud.anchorPoint = CGPoint(x: 0.5, y: 0.5);
-            cloud.position = CGPoint(x: cloud.size.width * CGFloat(i) / 3 - 100, y: cloud.size.height * CGFloat(i) / 2 - 150);
+            
+            // used to randomly determine x position of clouds
+            let offsetXValue = Int.random(min: 100, max: 500);
+            x -= CGFloat(offsetXValue);
+
+            cloud.position = CGPoint(x: x, y: y);
             cloud.zPosition = 1;
-            cloud.setScale(0.75);
+            
+            // used to randomly determine size of clouds
+            let cloudSizeRandom = Int.random(min: 0, max: 3);
+            let cloudSize = cloudSizeArray[cloudSizeRandom];
+            cloud.setScale(cloudSize);
+            
             self.addChild(cloud);
+            
+            // used to randomly determine y position of clouds
+            let offsetYValue = Int.random(min: 10, max: 80);
+            y -= self.frame.size.height/4 + CGFloat(offsetYValue);
         }
     }
     
+    // same as move mountains function in GameScene
     func moveClouds() {
         
         enumerateChildNodes(withName: "cloud") {
@@ -67,7 +88,7 @@ class BonusScene: SKScene, SKPhysicsContactDelegate {
             
             cloudsNode.position.x -= 0.5;
             
-            // less than because mountains are scrolling left
+            // less than because clouds are scrolling left
             if cloudsNode.position.x < -(self.frame.size.width) {
                 cloudsNode.position.x += self.frame.size.width * 2;
             }
